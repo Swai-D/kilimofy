1e65024f2e2b4002abfa257b316786c76931afb6
Swai-D <davyswai@gmail.com>
2021-10-23 17:47:51 +0300

    seeder updates

--- database/seeds/ForumTableSeeder.php
+++ database/seeds/ForumTableSeeder.php
@@ -16,8 +16,8 @@ class ForumTableSeeder extends Seeder
           'Category' => 'Kilimo',
           'Category_Description' => 'A place for the community to talk and chat about agriculture! ',
           'Category_Image' => '1633429307.png',
-          'Topics' => 0,
-          'Posts' => 0,
+          'Topics' => 3,
+          'Posts' => 3,
           'created_at' => '2021-09-27 08:30:18',
           'updated_at' => '2021-10-12 02:24:30',
       ]);
@@ -27,8 +27,8 @@ class ForumTableSeeder extends Seeder
           'Category' => 'Usafirishaji',
           'Category_Description' => 'Everything about the transportation of agriculture products',
           'Category_Image' => '1633814279.png',
-          'Topics' => 0,
-          'Posts' => 0,
+          'Topics' => 2,
+          'Posts' => 2,
           'created_at' => '2021-09-27 08:30:18',
           'updated_at' => '2021-10-12 02:24:30',
       ]);
